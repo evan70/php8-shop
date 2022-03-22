b1eb90e4fbb67c8316cb7a27f7e4002a370cae37
evan70 <ivanpaldan@yandex.com>
2022-03-22 20:20:45 +0100

    new file:   app/controllers/LanguageController.php 	modified:   config/routes.php 	modified:   public/assets/js/main.js 	modified:   tmp/logs/errors.log 	modified:   vendor/wfm/helpers/functions.php

--- vendor/wfm/helpers/functions.php
+++ vendor/wfm/helpers/functions.php
@@ -12,3 +12,14 @@ function h($str)
 {
     return htmlspecialchars($str);
 }
+
+function redirect($http = false)
+{
+    if ($http) {
+        $redirect = $http;
+    } else {
+        $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PATH;
+    }
+    header("Location: $redirect");
+    die;
+}

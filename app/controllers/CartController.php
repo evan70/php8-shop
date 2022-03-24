d518a484ba9bd3888ef8a0c1f0b1eb0477aae342
evan70 <ivanpaldan@yandex.com>
2022-03-24 23:51:27 +0100

    modified:   app/controllers/CartController.php 	modified:   app/models/Cart.php 	modified:   app/views/Main/index.php 	modified:   vendor/wfm/App.php 	modified:   vendor/wfm/Controller.php

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -11,7 +11,7 @@ use wfm\App;
 class CartController extends AppController
 {
 
-    public function addAction()
+    public function addAction(): bool
     {
         $lang = App::$app->getProperty('language');
         $id = get('id');
@@ -22,7 +22,16 @@ class CartController extends AppController
         }
 
         $product = $this->model->get_product($id, $lang);
-        debug($product,1);
+        if (!$product) {
+            return false;
+        }
+
+        $this->model->add_to_cart($product, $qty);
+        if ($this->isAjax()) {
+            debug($_SESSION['cart'],1);
+        }
+        redirect();
+        return true;
     }
 
 }

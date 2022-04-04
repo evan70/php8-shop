23660a7482927e850887494e310c406a923819d8
evan70 <ivanpaldan@yandex.com>
2022-04-04 23:56:29 +0200

    modified:   app/controllers/UserController.php 	modified:   app/models/User.php 	modified:   app/views/User/signup.php 	modified:   tmp/cache/2953c6ae6820d007c488f1c991e91b06.txt 	modified:   tmp/cache/6ef8f8468157405a8bd94397ab1a23a4.txt 	modified:   tmp/logs/errors.log 	modified:   vendor/wfm/Model.php 	modified:   vendor/wfm/helpers/functions.php

--- vendor/wfm/Model.php
+++ vendor/wfm/Model.php
@@ -4,6 +4,7 @@
 namespace wfm;
 
 
+use RedBeanPHP\R;
 use Valitron\Validator;
 
 abstract class Model
@@ -62,6 +63,17 @@ abstract class Model
             $labels[$k] = ___($v);
         }
         return $labels;
+    }
+
+    public function save($table): int|string
+    {
+        $tbl = R::dispense($table);
+        foreach ($this->attributes as $name => $value) {
+            if ($value != '') {
+                $tbl->$name = $value;
+            }
+        }
+        return R::store($tbl);
     }
 
 }

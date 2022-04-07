9a35577b828a468a280b49c9db860cda3f1e7b15
evan70 <ivanpaldan@yandex.com>
2022-04-07 23:12:58 +0200

    modified:   app/controllers/CartController.php 	modified:   app/models/Order.php 	modified:   composer.json 	modified:   composer.lock 	modified:   config/params.php 	modified:   tmp/cache/13155f5134df6153ed6b575001dfeb6c.txt 	modified:   tmp/cache/2953c6ae6820d007c488f1c991e91b06.txt 	modified:   tmp/cache/6ef8f8468157405a8bd94397ab1a23a4.txt 	modified:   tmp/cache/f95c605ee46f2eb0adab36530ffa46d6.txt 	modified:   tmp/logs/errors.log

--- config/params.php
+++ config/params.php
@@ -4,12 +4,4 @@ return [
     'admin_email' => 'admin@new-ishop.loc',
     'site_name' => 'E-Shop',
     'pagination' => 3,
-
-    'smtp_host' => 'smtp.mailtrap.io',
-    'smtp_auth' => true,
-    'smtp_port' => 2525,
-    'smtp_username' => '851414cc03c80e',
-    'smtp_password' => '91de611eb0901d',
-    'smtp_secure' => null, //ssl
-    'smtp_from_email' => '722d9f4dad-61c669@inbox.mailtrap.io',
 ];

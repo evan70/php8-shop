7672c88978bd96d44de4106574f90ebe4abcffc1
evan70 <ivanpaldan@yandex.com>
2022-04-07 23:13:57 +0200

    modified:   app/models/Order.php 	modified:   tmp/cache/2953c6ae6820d007c488f1c991e91b06.txt 	modified:   tmp/cache/6ef8f8468157405a8bd94397ab1a23a4.txt 	modified:   tmp/logs/errors.log 	modified:   vendor/composer/autoload_psr4.php 	modified:   vendor/composer/autoload_static.php 	modified:   vendor/composer/installed.json 	modified:   vendor/composer/installed.php 	modified:   vendor/composer/platform_check.php

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -10,5 +10,4 @@ return array(
     'app\\' => array($baseDir . '/app'),
     'Valitron\\' => array($vendorDir . '/vlucas/valitron/src/Valitron'),
     'RedBeanPHP\\' => array($vendorDir . '/gabordemooij/redbean/RedBeanPHP'),
-    'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
 );

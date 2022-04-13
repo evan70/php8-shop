5bfc861fe823f217f61920ede21e5a2e4b40e5b3
evan70 <ivanpaldan@yandex.com>
2022-04-13 22:41:31 +0200

    modified:   app/controllers/admin/AppController.php 	modified:   app/controllers/admin/MainController.php 	modified:   app/views/admin/Main/index.php 	modified:   app/views/admin/parts/sidebar.php

--- app/controllers/admin/AppController.php
+++ app/controllers/admin/AppController.php
@@ -4,7 +4,6 @@
 namespace app\controllers\admin;
 
 
-use app\models\admin\User;
 use app\models\AppModel;
 use app\widgets\language\Language;
 use wfm\App;
@@ -18,11 +17,6 @@ class AppController extends Controller
     public function __construct($route)
     {
         parent::__construct($route);
-
-        if (!User::isAdmin() && $route['action'] != 'login-admin') {
-            redirect(ADMIN . '/user/login-admin');
-        }
-
         new AppModel();
         App::$app->setProperty('languages', Language::getLanguages());
         App::$app->setProperty('language', Language::getLanguage(App::$app->getProperty('languages')));

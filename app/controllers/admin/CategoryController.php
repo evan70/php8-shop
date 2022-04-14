1390bfe7feb5f357e72355923d4a18719ff3c9e7
evan70 <ivanpaldan@yandex.com>
2022-04-14 21:26:06 +0200

    modified:   app/controllers/admin/CategoryController.php

--- app/controllers/admin/CategoryController.php
+++ app/controllers/admin/CategoryController.php
@@ -4,6 +4,8 @@
 namespace app\controllers\admin;
 
 
+use RedBeanPHP\R;
+
 class CategoryController extends AppController
 {
 
@@ -12,6 +14,28 @@ class CategoryController extends AppController
         $title = 'Категории';
         $this->setMeta("Админка :: {$title}");
         $this->set(compact('title'));
+    }
+
+    public function deleteAction()
+    {
+        $id = get('id');
+        $errors = '';
+        $children = R::count('category', 'parent_id = ?', [$id]);
+        $products = R::count('product', 'category_id = ?', [$id]);
+        if ($children) {
+            $errors .= 'Ошибка! В категории есть вложенные категории<br>';
+        }
+        if ($products) {
+            $errors .= 'Ошибка! В категории есть товары<br>';
+        }
+        if ($errors) {
+            $_SESSION['errors'] = $errors;
+        } else {
+            R::exec("DELETE FROM category WHERE id = ?", [$id]);
+            R::exec("DELETE FROM category_description WHERE category_id = ?", [$id]);
+            $_SESSION['success'] = 'Категория удалена';
+        }
+        redirect();
     }
 
 }

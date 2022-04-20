8636994331e1b3353ea8b06abdf8426c3b270e56
evan70 <ivanpaldan@yandex.com>
2022-04-20 23:44:02 +0200

    modified:   app/controllers/admin/UserController.php 	modified:   app/models/admin/User.php 	new file:   app/views/admin/User/index.php 	new file:   app/views/admin/User/view.php

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -4,10 +4,46 @@
 namespace app\controllers\admin;
 
 use app\models\admin\User;
+use RedBeanPHP\R;
+use wfm\Pagination;
 
 /** @property User $model */
 class UserController extends AppController
 {
+
+    public function indexAction()
+    {
+        $page = get('page');
+        $perpage = 20;
+        $total = R::count('user');
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+
+        $users = $this->model->get_users($start, $perpage);
+        $title = 'Список пользователей';
+        $this->setMeta("Админка :: {$title}");
+        $this->set(compact('title', 'users', 'pagination', 'total'));
+    }
+
+    public function viewAction()
+    {
+        $id = get('id');
+        $user = $this->model->get_user($id);
+        if (!$user) {
+            throw new \Exception('Not founud user', 404);
+        }
+
+        $page = get('page');
+        $perpage = 1;
+        $total = $this->model->get_count_orders($id);
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+
+        $orders = $this->model->get_user_orders($start, $perpage, $id);
+        $title = 'Профиль пользователя';
+        $this->setMeta("Админка :: {$title}");
+        $this->set(compact('title', 'user', 'pagination', 'total', 'orders'));
+    }
 
     public function loginAdminAction()
     {
--- app/models/admin/User.php
+++ app/models/admin/User.php
@@ -2,12 +2,24 @@
 
 namespace app\models\admin;
 
+use RedBeanPHP\R;
+
 class User extends \app\models\User
 {
 
     public static function isAdmin(): bool
     {
         return (isset($_SESSION['user']) && $_SESSION['user']['role'] == 'admin');
+    }
+
+    public function get_users($start, $perpage): array
+    {
+        return R::findAll("user", "LIMIT $start, $perpage");
+    }
+
+    public function get_user($id): array
+    {
+        return R::getRow("SELECT * FROM user WHERE id = ?", [$id]);
     }
 
 }

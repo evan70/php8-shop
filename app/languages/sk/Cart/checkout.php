8f7d5eccd44561360927e88241f79bddba71e13a
evan70 <ivanpaldan@yandex.com>
2022-05-03 23:36:49 +0200

    new file:   app/languages/sk/Cart/checkout.php

---
+++ app/languages/sk/Cart/checkout.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'cart_checkout_email_input' => 'E-mail',
+    'cart_checkout_password_input' => 'Heslo',
+    'cart_checkout_name_input' => 'Meno',
+    'cart_checkout_address_input' => 'Adresa',
+    'cart_checkout_note_input' => 'Poznámka',
+    'cart_checkout_error_register' => 'Chyba registrácie',
+    'cart_checkout_error_email_unique' => 'Tento email už bol registrovaný',
+    'cart_checkout_error_save_order' => 'Chyba potvrdenia objednávky оформления заказа',
+
+    'cart_checkout_mail_subject' => 'Objednávka č.%d',
+    'cart_checkout_order_success' => 'Ďakujeme za objednávku. V najbližšej dobe Vás budeme kontaktovať o stave objednávky',
+];

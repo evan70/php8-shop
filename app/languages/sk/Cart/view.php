dd0a224efb916d5fd9bae2d85f2e22d1abcf4310
evan70 <ivanpaldan@yandex.com>
2022-05-03 23:30:17 +0200

    new file:   app/languages/sk/Cart/view.php

---
+++ app/languages/sk/Cart/view.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'cart_view_email_input' => 'E-mail',
+    'cart_view_password_input' => 'Heslo',
+    'cart_view_name_input' => 'Meno',
+    'cart_view_address_input' => 'Adresa',
+    'cart_view_note_input' => 'Poznámka',
+    'cart_view_order_btn' => 'Potvrdiť objednávku',
+];

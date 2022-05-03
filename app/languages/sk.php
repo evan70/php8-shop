c868af95e7437403e963b7fa2b45c3fb83770083
evan70 <ivanpaldan@yandex.com>
2022-05-03 23:24:29 +0200

    new file:   app/languages/sk.php

---
+++ app/languages/sk.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    'tpl_information' => 'Info',
+    'tpl_work_hours' => 'Pracovná doba',
+    'tpl_contacts' => 'Kontakty',
+    'tpl_we_online' => 'Sociálne siete',
+    'tpl_search' => 'Hľadať...',
+    'tpl_home_link' => 'Domov',
+
+    'tpl_home_breadcrumbs' => '<i class="fas fa-home"></i>',
+    'tpl_total_pagination' => 'tovar(ov) z',
+
+    'tpl_cart_title' => 'Košík',
+    'tpl_cart_photo' => 'Foto',
+    'tpl_cart_product' => 'Tovar',
+    'tpl_cart_qty' => 'Množstvo',
+    'tpl_cart_price' => 'Cena',
+    'tpl_cart_total_qty' => 'Položiek spolu:',
+    'tpl_cart_sum' => 'Spolu:',
+    'tpl_cart_btn_continue' => 'Pokračovať v nákupe',
+    'tpl_cart_btn_order' => 'Potvrdiť objednávku',
+    'tpl_cart_btn_clear' => 'Vymazať košík',
+    'tpl_cart_empty' => 'Košík je prázdny',
+
+    'tpl_login' => 'Prihlásenie',
+    'tpl_signup' => 'Registrácia',
+    'tpl_logout' => 'Odhlásenie',
+    'tpl_cabinet' => 'Kabinet',
+
+    'tpl_orders' => 'Objednávky',
+    'tpl_orders_files' => 'Súbory',
+    'tpl_user_credentials' => 'Vaše údaje',
+    'tpl_user_logout' => 'Odhlásenie',
+
+    'tpl_signup_email_input' => 'E-mail',
+    'tpl_signup_password_input' => 'Heslo',
+    'tpl_signup_name_input' => 'Meno',
+    'tpl_signup_address_input' => 'Adresa',
+
+    'tpl_search_title' => 'Hľadať',
+    'tpl_search_query' => 'Hľadať podľa: ',
+    'tpl_search_not_found' => 'Vaša požiadavka nenájdená',
+
+    'tpl_wishlist_add_success' => 'Tovar bol pridaný k obľúbeným položkám',
+    'tpl_wishlist_add_error' => 'Chyba - tovar nebol pridaný k obľúbeným položkám',
+    'tpl_wishlist_delete_success' => 'Tovar bol vynazaný z obľúbených položiek',
+    'tpl_wishlist_delete_error' => 'Chyba vynazania z obľúbených položiek',
+
+    'tpl_error_404' => '404 - Stránka nenájdená',
+];

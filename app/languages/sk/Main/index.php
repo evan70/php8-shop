3f35d9215f1f8e6466887f8b985f2ebfa72371cd
evan70 <ivanpaldan@yandex.com>
2022-05-03 23:27:09 +0200

    new file:   app/languages/sk/Main/index.php

---
+++ app/languages/sk/Main/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'main_index_meta_title' => 'Hlavná stránka',
+    'main_index_meta_description' => 'Meta popis',
+    'main_index_meta_keywords' => 'Kľúčové slová ...',
+    'main_index_featured_products' => 'Odporúčaný tovar',
+];

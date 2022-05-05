97d24e0885cb5dff8f30ccc04ceb5b928510a9bf
evan70 <ivanpaldan@yandex.com>
2022-05-05 23:41:58 +0200

    new file:   app/languages/sk/User/credentials.php

---
+++ app/languages/sk/User/credentials.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'user_credentials_title' => 'Fakturačné údaje',
+    'user_credentials_save_btn' => 'Uložiť',
+    'user_credentials_success' => 'Údaje boli uložené',
+    'user_credentials_error' => 'Chyba uloženia údajov',
+];

832a2bd5a767800676504ad5f3c74612e4706bb3
evan70 <ivanpaldan@yandex.com>
2022-05-05 23:46:27 +0200

    new file:   app/languages/sk/User/login.php

---
+++ app/languages/sk/User/login.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'user_login_login_btn' => 'Vstup',
+    'user_login_success_login' => 'Boli ste úspešne autorizovaný',
+    'user_login_error_login' => 'Nesúhlasí heslo alebo login',
+];

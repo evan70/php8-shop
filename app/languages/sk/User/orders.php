7350cbd5abff35c7d65ef85a7840011a74ca9be9
evan70 <ivanpaldan@yandex.com>
2022-05-06 23:10:40 +0200

    new file:   app/languages/sk/User/orders.php

---
+++ app/languages/sk/User/orders.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'user_orders_title' => 'Objednávky',
+    'user_orders_empty' => 'Nemáte žiadnu objednávku',
+    'user_orders_num' => 'č. objednávky',
+    'user_orders_status' => 'Stav',
+    'user_orders_total' => 'Suma objednávok',
+    'user_orders_created' => 'Dátum vytvorenia',
+    'user_orders_updated' => 'Dátum zmeny',
+    'user_orders_total_pagination' => 'objednávk(a/ok) z',
+    'user_order_status_0' => 'Nová',
+    'user_order_status_1' => 'Vybavená',
+];

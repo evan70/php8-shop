6311da5f5c2c02ddade825f495d2d540dabb13d2
evan70 <ivanpaldan@yandex.com>
2022-05-06 23:13:40 +0200

    new file:   app/languages/sk/User/signup.php

---
+++ app/languages/sk/User/signup.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'user_signup_signup_btn' => 'Zaregistrovať sa',
+    'user_signup_success_register' => 'Registrácia bola úspešná',
+    'user_signup_error_register' => 'Chyba registrácie',
+    'user_signup_error_email_unique' => 'e-mail už bol registrovaný',
+];

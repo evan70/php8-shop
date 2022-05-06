bc904a00490de22a50c544c5e3d5db1a2e5bbeae
evan70 <ivanpaldan@yandex.com>
2022-05-06 23:17:56 +0200

    new file:   app/languages/sk/Search/index.php

---
+++ app/languages/sk/Search/index.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'search_index_not_found' => 'Vaša požiadavka nebola nájdená',
+    'search_index_search_title' => 'Vyhľadávanie',
+    'search_index_search_query' => 'Vyhľadávanie podľa: ',
+];

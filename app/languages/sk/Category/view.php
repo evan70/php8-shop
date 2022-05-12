1e47fd22b386e91c0dfbb2362c2c11ba69567663
evan70 <ivanpaldan@yandex.com>
2022-05-12 23:37:53 +0200

    modified:   app/languages/sk/Category/view.php

--- app/languages/sk/Category/view.php
+++ app/languages/sk/Category/view.php
@@ -3,8 +3,8 @@
 return [
     'category_view_no_products' => 'Kategória neobsahuje žiadne produkty',
     'category_view_home_breadcrumbs' => '<i class="fas fa-home"></i>',
-    'category_view_total_pagination' => 'товара(ов) из',
-    'category_view_sort' => 'Сортировка',
+    'category_view_total_pagination' => 'produkt(ov) z',
+    'category_view_sort' => 'Filter',
     'category_view_sort_by_default' => 'По умолчанию',
     'category_view_sort_title_asc' => 'Название (А - Я)',
     'category_view_sort_title_desc' => 'Название (Я - А)',

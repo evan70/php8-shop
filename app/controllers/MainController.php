38799eb0553c0e100b108d44268338525875dc4c
evan70 <ivanpaldan@yandex.com>
2022-08-16 22:04:06 +0200

    modified:   app/controllers/MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,12 +2,10 @@
 
 namespace app\controllers;
 
-
 use app\models\Main;
 use RedBeanPHP\R;
 use wfm\App;
 use wfm\Cache;
-
 
 /** @property Main $model */
 class MainController extends AppController
